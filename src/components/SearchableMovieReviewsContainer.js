2ad2aba1bd1641c420f8c56f9e17ea666ea0093d
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-17 13:24:32 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,32 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    search: '',
+    revs: []
+  };
+
+  handleSearch = event =>
+    this.setState({ search: event.target.value });
+
+  Submit = event => {
+    event.preventDefault();
+
+    fetch(BASE_URL.concat(this.state.search))
+      .then(res => res.json())
+      .then(josn => this.setState({ revs: josn.results }));
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.Submit}>
+          <input id="search" type="text" onChange={this.handleSearch}/>
+          <button type="submit">Submit</button>
+        </form>
+        <MovieReviews revs={this.state.revs} />
+      </div>
+    );
+  }
+}
